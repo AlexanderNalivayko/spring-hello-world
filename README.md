9b4cbe961d60e690e0cbc1d9571f0278a750f7ec
Alexander <nlvk48@gmail.com>
2019-08-11 23:47:56 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple Spring hello world project.
---
+++ src/main/java/com/alex/spring/GreetingController.java
@@ -0,0 +1,13 @@
+package com.alex.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GreetingController {
+
+    @RequestMapping("/hi")
+    public String greeting(){
+        return "hello.html";
+    }
+}
